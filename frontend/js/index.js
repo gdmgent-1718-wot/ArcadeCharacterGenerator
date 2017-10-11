0ab1c4a4977f1d42d40a3c5c2bc3a06a4dbe7b34
Adriaan Glibert <adriglib@student.arteveldehs.be>
2017-10-11 12:01:01 +0200

    vuecolor test

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,4 @@
+
 function createMatrix(row, col){
     a = []
     for(var i=0; i < row; i++){
@@ -15,7 +16,7 @@ new Vue({
 	el: '#app',
   data: {
     matrix: matrixConstruction,
-    color: 'rgb(220,220,220)'
+    colors: 'rgb(220,220,220)'
   },
   methods:{
     handleClick: function(index, parent, event){
@@ -30,3 +31,37 @@ new Vue({
     }
   }
 });
+
+
+var defaultProps = {
+    hex: '#194d33',
+    hsl: {
+        h: 150,
+        s: 0.5,
+        l: 0.2,
+        a: 1
+    },
+    hsv: {
+        h: 150,
+        s: 0.66,
+        v: 0.30,
+        a: 1
+    },
+    rgba: {
+        r: 25,
+        g: 77,
+        b: 51,
+        a: 1
+    },
+    a: 1
+}
+
+new Vue({
+    el: '#app',
+    components: {
+        'chrome-picker': chrome
+    },
+    data: {
+        colors: defaultProps
+    }
+})
